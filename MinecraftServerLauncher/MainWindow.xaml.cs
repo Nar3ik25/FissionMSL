0e1e1b877c6632a8f22834b0ff023ce6c5f801f3
Nar3ik25 <kierankelly2016@outlook.com>
2025-05-16 01:57:40 -0700

    Changed internal version number
    Changed the internal version number to be more consistent with my numbering scheme.

--- MinecraftServerLauncher/MainWindow.xaml.cs
+++ MinecraftServerLauncher/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace MinecraftServerLauncher
         public static readonly string ApplicationDataPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\FissionMSL\";
 
         // The application version number (Must be changed for each update!)
-        public static readonly Version InstalledVersion = new Version(0, 2, 1, 1);
+        public static readonly Version InstalledVersion = new Version(0, 2, 2, 0);
 
         // Java path for the application.
         public static string ApplicationJavaPath = null;
